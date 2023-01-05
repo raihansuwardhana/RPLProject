cc5c047ef1940ec7b8abd4d61dc51b78aa0124e6
unknown <raihansuwardhanaarisyi@gmail.com>
2023-01-05 21:09:55 +0700

    Projekpertama

---
+++ TB1/src/view/ReportView.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+/**
+ *
+ * @author kristanti
+ */
+public class ReportView {
+    public static void main (String [] args){
+        new reportview1().setVisible(true);
+    }
+}
